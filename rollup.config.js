faa13281ac443ff005d94a161a56d4673e8ccb2b
Mhdi <0MehdiKeramati1@gmail.com>
2024-07-15 13:51:13 +0330

    1.0.1

--- rollup.config.js
+++ rollup.config.js
@@ -10,10 +10,12 @@ export default {
     {
       file: "dist/index.js",
       format: "cjs",
+      sourcemap: true,
     },
     {
       file: "dist/index.esm.js",
       format: "esm",
+      sourcemap: true,
     },
   ],
   plugins: [
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,1 @@
 export { default as ImageGallery } from "./components/ImageGallery";
-export type { ImageGalleryProps } from './types';   
-// export { default as AlbumModal } from "./components/AlbumModal";
-// export { default as SingleImageModal } from "./components/SingleImageModal";
